c5f108976627b50289c6cc909439e764a7bb5dac
ioBroker Adapter Creator <noreply@iobroker.net>
2024-10-05 21:03:50 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,31 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+"use strict";
+
+systemDictionary = {
+    "linky adapter settings": {
+        "en": "Adapter settings for linky",
+        "de": "Adaptereinstellungen für linky",
+        "ru": "Настройки адаптера для linky",
+        "pt": "Configurações do adaptador para linky",
+        "nl": "Adapterinstellingen voor linky",
+        "fr": "Paramètres d'adaptateur pour linky",
+        "it": "Impostazioni dell'adattatore per linky",
+        "es": "Ajustes del adaptador para linky",
+        "pl": "Ustawienia adaptera dla linky",
+        "zh-cn": "linky的适配器设置"
+    },
+    "token_enedis": {
+        "en": "TOKEN",
+        "de": "TOKEN",
+        "ru": "TOKEN",
+        "pt": "TOKEN",
+        "nl": "TOKEN",
+        "fr": "TOKEN",
+        "it": "TOKEN",
+        "es": "TOKEN",
+        "pl": "TOKEN",
+        "zh-cn": "TOKEN"
+    }
+};
